87e3ed2d289ddf98fa0a4a598e1bff7b174b4f90
HYBAS22 <hybas22@gmail.com>
2024-06-08 14:38:58 +0300

    Добавлено аппаратное ускорение, код был немного упорядочен и улучшен

--- Video Compressor Online Without Registration/MainWindow.xaml.cs
+++ Video Compressor Online Without Registration/MainWindow.xaml.cs
@@ -1,6 +1,6 @@
 ﻿using System;
 using System.Diagnostics;
-using System.Globalization;
+using System.IO;
 using System.Text.RegularExpressions;
 using System.Threading.Tasks;
 using System.Windows;
@@ -8,21 +8,18 @@ using NReco.VideoInfo;
 
 namespace Video_Compressor_Online_Without_Registration
 {
-
-    /// <summary>
-    /// Interaction logic for MainWindow.xaml
-    /// </summary>
     public partial class MainWindow
     {
+        private readonly FFProbe _ffProbe = new FFProbe();
+
         public MainWindow()
         {
             InitializeComponent();
         }
 
-        double GetVideoDuration(string videoFilePath)
+        private double GetVideoDuration(string videoFilePath)
         {
-            var ffProbe = new FFProbe();
-            MediaInfo mediaInfo = ffProbe.GetMediaInfo(videoFilePath);
+            MediaInfo mediaInfo = _ffProbe.GetMediaInfo(videoFilePath);
 
             if (mediaInfo != null && mediaInfo.Duration != TimeSpan.Zero)
             {
@@ -30,151 +27,192 @@ namespace Video_Compressor_Online_Without_Registration
             }
             return 0; // Если не удалось получить длительность
         }
-
-        public async void CompressVideo(string videoFilePath, string outputFilePath, double targetSizeMB)
+        
+        // Определяем, поддерживается ли выбранный кодек на текущей системе
+        bool IsCodecSupported(string codec)
         {
-            // Получим информацию о видео
-            var ffProbe = new FFProbe();
-            MediaInfo mediaInfo = ffProbe.GetMediaInfo(videoFilePath);
-
-            // Получим длительность видео
-            double totalVideoDurationInSeconds = mediaInfo.Duration.TotalSeconds;
-
-            // Пересчитаем целевой размер в байтах
-            long targetSizeBytes = (long)(targetSizeMB * 1024 * 1024);
-
-            // Определим целевой битрейт видео и аудио
-            long videoBitrate = (long)((targetSizeBytes * 0.9 * 8) / totalVideoDurationInSeconds); // 90% на видео
-            long audioBitrate = (long)((targetSizeBytes * 0.1 * 8) / totalVideoDurationInSeconds); // 10% на аудио
-
-            // Выполним сжатие видео
             var ffMpegProcess = new Process
             {
                 StartInfo = new ProcessStartInfo
                 {
-                    FileName = "ffmpeg.exe", // Путь к исполняемому файлу FFmpeg
-                    Arguments = $"-i \"{videoFilePath}\" -b:v {videoBitrate} -b:a {audioBitrate} \"{outputFilePath}\"",
-                    RedirectStandardError = true,
+                    FileName = "ffmpeg.exe",
+                    Arguments = $"-hide_banner -codecs",
+                    RedirectStandardOutput = true,
                     UseShellExecute = false,
                     CreateNoWindow = true
                 }
             };
 
-            ffMpegProcess.ErrorDataReceived += (s, args) =>
+            ffMpegProcess.Start();
+            string output = ffMpegProcess.StandardOutput.ReadToEnd();
+            ffMpegProcess.WaitForExit();
+
+            return output.Contains(codec);
+        }
+
+        // Выбираем подходящее аппаратное ускорение для кодирования видео
+        string GetHardwareAcceleration(string codec)
+        {
+            if (codec == "h264" || codec == "h265")
             {
-                if (args.Data != null)
+                if (IsCodecSupported("nvenc"))
                 {
-                    string line = args.Data;
-                    if (line.Contains("time="))
+                    return "-c:v h264_nvenc";
+                }
+                else if (IsCodecSupported("h264_qsv"))
+                {
+                    return "-c:v h264_qsv";
+                }
+                else if (IsCodecSupported("h264_amf"))
+                {
+                    return "-c:v h264_amf";
+                }
+            }
+            return ""; // Возвращаем пустую строку, если подходящее аппаратное ускорение не найдено
+        }
+
+        public async Task CompressVideoAsync(string videoFilePath, string outputFilePath, long targetSizeMb)
+        {
+            try
+            {
+                // Получим информацию о видео
+                var ffProbe = new FFProbe();
+                MediaInfo mediaInfo = ffProbe.GetMediaInfo(videoFilePath);
+
+                // Получим длительность видео
+                double totalVideoDurationInSeconds = mediaInfo.Duration.TotalSeconds;
+
+                // Пересчитаем целевой размер в байтах
+                long targetSizeBytes = targetSizeMb * 1024 * 1024;
+
+                // Определим целевой битрейт видео и аудио
+                long videoBitrate = (long) ((targetSizeBytes * 0.9 * 8) / totalVideoDurationInSeconds); // 90% на видео
+                long audioBitrate = (long) ((targetSizeBytes * 0.1 * 8) / totalVideoDurationInSeconds); // 10% на аудио
+
+                // Выбираем подходящее аппаратное ускорение для выбранного кодека
+                string hardwareAcceleration = GetHardwareAcceleration(mediaInfo.Streams[0].CodecName);
+
+                // Выполним сжатие видео
+                var ffMpegProcess = new Process
+                {
+                    StartInfo = new ProcessStartInfo
                     {
-                        Match match = Regex.Match(line, @"time=([0-9:.]+)");
-                        if (match.Success)
+                        FileName = "ffmpeg.exe",
+                        Arguments =
+                            $"-y -i \"{videoFilePath}\" {hardwareAcceleration} -b:v {videoBitrate} -b:a {audioBitrate} -threads 4 \"{outputFilePath}\"",
+                        RedirectStandardError = true,
+                        UseShellExecute = false,
+                        CreateNoWindow = true
+                    }
+                };
+
+                ffMpegProcess.ErrorDataReceived += (s, args) =>
+                {
+                    if (args.Data != null)
+                    {
+                        string line = args.Data;
+                        if (line.Contains("time="))
                         {
-                            string time = match.Groups[1].Value;
-                            TimeSpan duration = TimeSpan.Parse(time);
-
-                            if (totalVideoDurationInSeconds > 0 && !double.IsNaN(duration.TotalSeconds) && !double.IsInfinity(duration.TotalSeconds))
+                            Match match = Regex.Match(line, @"time=([0-9:.]+)");
+                            if (match.Success)
                             {
-                                double progress = duration.TotalSeconds / totalVideoDurationInSeconds * 100.0;
+                                string time = match.Groups[1].Value;
+                                TimeSpan duration = TimeSpan.Parse(time);
 
-                                Dispatcher.Invoke(() =>
+                                if (totalVideoDurationInSeconds > 0 && !double.IsNaN(duration.TotalSeconds) &&
+                                    !double.IsInfinity(duration.TotalSeconds))
                                 {
-                                    convertationProgress.Value = progress;
-                                });
+                                    double progress = duration.TotalSeconds / totalVideoDurationInSeconds * 100.0;
+
+                                    Dispatcher.Invoke(() => { convertationProgress.Value = progress; });
+                                }
                             }
                         }
                     }
-                }
-            };
+                };
 
-            ffMpegProcess.Exited += (s, args) =>
-            {
-                Dispatcher.Invoke(() =>
+                ffMpegProcess.Exited += (s, args) =>
                 {
-                    convertationProgress.Value = 100;
-                });
-            };
+                    Dispatcher.Invoke(() => { convertationProgress.Value = 100; });
+                };
 
-            ffMpegProcess.Start();
-            ffMpegProcess.BeginErrorReadLine();
+                ffMpegProcess.Start();
+                ffMpegProcess.BeginErrorReadLine();
 
-            await Task.Run(() =>
+                await Task.Run(() => ffMpegProcess.WaitForExit());
+
+                MessageBox.Show("Видео было успешно сжато и сохранено вместе с оригиналом в том же месте.");
+
+                ResetUI();
+            }
+            
+            catch (Exception ex)
             {
-                ffMpegProcess.WaitForExit();
-                MessageBox.Show("Видео было успешно сжато и сохранено вместе с оригиналом в том же месте");
-            });
+                MessageBox.Show($"Произошла ошибка при сжатии видео: {ex.Message}", "Ошибка", MessageBoxButton.OK, MessageBoxImage.Error);
+            }
 
+        }
+
+        private void ResetUI()
+        {
             OriginalFilePath.Text = "";
             CompressedFilePath.Text = "";
             convertationProgress.Value = 0;
+            SizeOfFile.Text = "";
         }
 
-        private void Convert_OnClick(object sender, RoutedEventArgs e)
+        private async void Convert_OnClick(object sender, RoutedEventArgs e)
         {
             string videoFilePath = OriginalFilePath.Text;
             string outputFilePath = CompressedFilePath.Text;
-            double targetSizeInKb;
 
-            if (double.TryParse(SizeOfFile.Text, out double fileSize))
+            if (long.TryParse(SizeOfFile.Text, out long fileSize) && File.Exists(videoFilePath) && !string.IsNullOrEmpty(outputFilePath))
             {
-                targetSizeInKb = fileSize;
+                await CompressVideoAsync(videoFilePath, outputFilePath, fileSize);
             }
             else
             {
-                throw new Exception("Не удалось преобразовать размер в числовое значение");
+                MessageBox.Show("Не все параметры были указаны, или указаны верно", "Ошибка", MessageBoxButton.OK, MessageBoxImage.Error);
             }
-            
-            if (!string.IsNullOrEmpty(OriginalFilePath.Text)) 
-                CompressVideo(videoFilePath, outputFilePath, targetSizeInKb);
-            else
-                MessageBox.Show("Укажите размер сжатия файла", "Ошибка", MessageBoxButton.OK, MessageBoxImage.Error);
         }
-        
+
         private void OriginalFilePathButtonClick(object sender, RoutedEventArgs e)
         {
-            // Configure open file dialog box
-            var dialog = new Microsoft.Win32.OpenFileDialog();
-            dialog.DefaultExt = ".mp4"; // Default file extension
-            dialog.Filter = "Video Files (*.mp4, *.mov, *.avi, *.wmv, *.mpeg, *.m4v)|*.mp4;*.mov;*.avi;*.wmv;*.mpeg;*.m4v";
+            var dialog = new Microsoft.Win32.OpenFileDialog
+            {
+                DefaultExt = ".mp4", // Default file extension
+                Filter = "Video Files (*.mp4, *.mov, *.avi, *.wmv, *.mpeg, *.m4v)|*.mp4;*.mov;*.avi;*.wmv;*.mpeg;*.m4v"
+            };
 
-            // Show open file dialog box
             bool? result = dialog.ShowDialog();
-
-            // Process open file dialog box results
             if (result == true)
             {
-                // Open document
                 string filepath = dialog.FileName;
                 string filename = dialog.SafeFileName;
                 OriginalFilePath.Text = filepath;
-                CompressedFilePath.Text = filepath.Replace(filename.Split('.')[0], filename.Split('.')[0] + "_COMPRESSED");
+                CompressedFilePath.Text = Path.Combine(Path.GetDirectoryName(filepath), Path.GetFileNameWithoutExtension(filepath) + "_COMPRESSED" + Path.GetExtension(filepath));
             }
         }
-        
+
         private void CompressFilePathButtonClick(object sender, RoutedEventArgs e)
         {
             if (string.IsNullOrEmpty(OriginalFilePath.Text))
             {
                 MessageBox.Show("Для начала укажите оригинал файла.", "Ошибка", MessageBoxButton.OK, MessageBoxImage.Error);
+                return;
             }
 
-            else
+            var dialog = new Microsoft.Win32.SaveFileDialog
             {
-                // Configure save file dialog box
-                var dialog = new Microsoft.Win32.SaveFileDialog();
-                dialog.FileName = OriginalFilePath.Text; // Default file name
-                dialog.DefaultExt = ".mp4"; // Default file extension
-                dialog.Filter = "Video Files (*.mp4, *.mov, *.avi, *.wmv, *.mpeg, *.m4v)|*.mp4;*.mov;*.avi;*.wmv;*.mpeg;*.m4v";
+                FileName = Path.GetFileNameWithoutExtension(OriginalFilePath.Text) + "_COMPRESSED",
+                DefaultExt = ".mp4", // Default file extension
+                Filter = "Video Files (*.mp4, *.mov, *.avi, *.wmv, *.mpeg, *.m4v)|*.mp4;*.mov;*.avi;*.wmv;*.mpeg;*.m4v"
+            };
 
-                // Show save file dialog box
-                bool? result = dialog.ShowDialog();
-
-                // Process save file dialog box results
-                if (result == true)
-                {
-                    // Save document
-                    string filename = dialog.FileName;
-                }
+            bool? result = dialog.ShowDialog();
+            if (result == true)
+            {
+                CompressedFilePath.Text = dialog.FileName;
             }
         }
     }
